2a5a3938747cbf55eea4dda2d9574480eef3377a
Adrien <developpeuradrien@gmail.com>
2025-03-28 09:32:36 +0100

    Maquettage des appels reseaux en erreur

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 README
 
 J'ai fait l'appel réseau avec Retrofit.
-L'appel réseau est enrobé de la classe Result afin de gerer les erreurs.
+L'appel réseau est enrobé de la classe Result afin de gérer les erreurs.
 La structure suit des principes SOLID
-Les dépendances sont injectés par Hilt.
+Les dépendances sont injectées par Hilt.
+
+
+Changements à faire :
+utiliser les flavours pour
+Pour les réponses web mockées : les déplacer dans leurs fichiers à part.
+Dans le projet, toutes les exceptions ont été rangées dans la même action par le view model.
+Professionnellement, Les exceptions renvoyées doivent être gérés plus finement : s'agit t il d'une absence de reseau ? une erreur back end ?
+
+
+Note : Une clé d'API doit être gardée secrète.
+Je me suis contenté de ne pas la présenter dans le projet (un système plus robuste aux erreurs doit être présent dans un projet professionel)
+Veuillez l'ajouter à WeatherWebDataSource la clé.
--- app/src/main/java/m/adrien/exomindtest/data/datasource/WeatherWebDataSource.kt
+++ app/src/main/java/m/adrien/exomindtest/data/datasource/WeatherWebDataSource.kt
@@ -1,46 +1,61 @@
 package m.adrien.exomindtest.data.datasource
 
-import android.system.ErrnoException
 import android.util.Log
 import kotlinx.serialization.json.Json
 import m.adrien.exomindtest.data.model.WeatherJsonResponse
+import okhttp3.Interceptor
+import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import javax.inject.Inject
 
-class WeatherWebDataSource @Inject constructor(){
 
-    //TODO : mieux injecter ces attributs là
-    private val BASE_URL =
-        "https://api.openweathermap.org/data/2.5/"
+class WeatherWebDataSource @Inject constructor() {
+
+    private val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
     //Use Flavour to determin if it should set or not (for Prod)
     private val logging = HttpLoggingInterceptor().apply {
         setLevel(HttpLoggingInterceptor.Level.BODY)
     }
-    private val client = OkHttpClient.Builder().apply { addInterceptor(logging) }.build()
+    /* Maquettage de l'appel reseau
+    val rewrite_answer = Interceptor { chain ->
+        val originalResponse: Response = chain.proceed(chain.request())
+        originalResponse.newBuilder().body(
+            """{"cod": 401, "message": "This is a test error answer"}""".toResponseBody(
+                "application/json".toMediaTypeOrNull()
+            )
+        ).build()
+    }*/
+    private val client = OkHttpClient.Builder().apply {
+        addInterceptor(logging)
+        //addInterceptor(rewrite_answer)
+    }.build()
 
     private val appJson = Json { ignoreUnknownKeys = true }
 
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(appJson.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(BASE_URL)
-        .client(client)
-        .build()
+        .baseUrl(BASE_URL).client(client).build()
 
     private val retrofitService: WeatherApiService by lazy {
         retrofit.create(WeatherApiService::class.java)
     }
 
-    suspend fun callWeatherWeb(cityName: String) :  Result<WeatherJsonResponse>{
-        Log.d("micheldr","callWeatherWeb")
+    suspend fun callWeatherWeb(cityName: String): Result<WeatherJsonResponse> {
+        Log.d("micheldr", "callWeatherWeb")
         try {
             val response = retrofitService.getWeather(cityName, WeatherApiKey().apiKey)
             Log.d("micheldr", "post response")
             return Result.success(response)
-        } catch(e: Exception){
+        } catch (e: Exception) {
             return Result.failure(e)
         }
 
